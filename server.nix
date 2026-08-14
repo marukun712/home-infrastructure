2f52978b3b003b54617bef51e1bbc7efc7eb9608
marukun712 <marukun530@gmail.com>
2026-08-14 14:21:16 +0900

    akaz domainをodango.appに向ける

--- server.nix
+++ server.nix
@@ -206,7 +206,7 @@
 
         local-data = [
           ''"maimai.mode. IN A 10.0.10.2"''
-          ''"xn--l8js9b.xn--7i8h IN A 10.0.10.3"''
+          ''"xn--l8js9b.xn--7i8h IN CNAME odango.app"''
         ];
       };
 
